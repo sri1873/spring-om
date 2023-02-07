2eaffaa4d99f181f7285c74f184a6104c9a02c0d
Sri Kumar <kssrikumar180703@gmail.com>
2023-02-07 15:12:17 +0530

    added native query

--- src/main/java/com/spring/om/repositories/InventoryRepository.java
+++ src/main/java/com/spring/om/repositories/InventoryRepository.java
@@ -3,6 +3,8 @@ package com.spring.om.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.spring.om.entities.Inventory;
@@ -10,8 +12,10 @@ import com.spring.om.entities.Inventory;
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
 
-	Inventory findByUuid(UUID productId);
+	@Query(value = "SELECT * FROM product_inventory e where e.uuid == :id", nativeQuery = true)
+	Inventory findByUuid(@Param("id") UUID productId);
 
+	@Query(value = "DELETE FROM product_inventory e where e.uuid == ?1", nativeQuery = true)
 	void deleteByUuid(UUID productId);
 
 }
